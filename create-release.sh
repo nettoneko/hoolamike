c3e3338650a18be5aee010f25f3ddd144bbccb61
niedzwiedzw <niedzwiedzwo@gmail.com>
2025-01-05 04:15:52 +0100

    feat(ci): CI with automatic publishes

---
+++ create-release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+export CARGO_REGISTRY_TOKEN=$(cat ./crates-io.token)
+
+PACKAGE=hoolamike
+VERSION=$(cargo pkgid --package $PACKAGE | cut -d "#" -f2)
+TAG="v${VERSION}"
+
+echo "creating release for ${PACKAGE} ${TAG}"
+
+# cargo publish --package "${PACKAGE}"
+
+git tag -a "${TAG}" -m "release ${TAG} of ${PACKAGE}"
+
+git push origin $(git rev-parse --abbrev-ref HEAD) --tags
