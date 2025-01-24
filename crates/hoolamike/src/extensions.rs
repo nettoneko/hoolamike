3433d98ceea34122825171b096467ec7b7c8fbb8
niedzwiedzw <niedzwiedzwo@gmail.com>
2025-01-24 11:13:47 +0100

    fix: unused imports

--- crates/hoolamike/src/extensions.rs
+++ crates/hoolamike/src/extensions.rs
@@ -1,11 +1,2 @@
-use {
-    anyhow::Context,
-    std::{
-        fs::File,
-        io::{Seek, SeekFrom, Write},
-        path::Path,
-    },
-};
-
 pub mod fallout_new_vegas_4gb_patch;
 pub mod tale_of_two_wastelands_installer;
